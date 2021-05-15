d2de14e469bcfcf6f075e0fb362794bfe3176d5b
ganeshkode <49275234+ganeshkode@users.noreply.github.com>
2021-05-15 10:57:43 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-<br><br><br><br><br><br><br><br>
+
                <div class="box footer">
                   <footer class="line">
                      <div class="s-12 l-6">
-                        <p>Copyright 2019</p>
+                        <p>Copyright 2021</p>
                      </div>
                      <div class="s-12 l-6">
-                 
+                 Design and coding by SATYAM FALKE, HRITHIK BANSAL, GANESH KODE.
               </div>
                   </footer>
                </div>
