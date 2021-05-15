31e055400b316930b2f8318dec201c2e3a64eb17
ganeshkode <49275234+ganeshkode@users.noreply.github.com>
2021-05-15 10:55:38 +0530

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -16,47 +16,30 @@ include ("header.php");
 <form method=post action=contactus.php>
 <div class="card columns">
   <img src="image/img2.png" alt="John" style="width:100%">
-  <h1>Rutuja Jadhav</h1>
+  <h1>SATYAM FALKE</h1>
   <p class="title">Founder</p>
-  <p>College : BHAUSAHEB VARTAK POLYTECHNIC</p>
+  <p>College : G H RAISONI INSTITUTE OF ENGINEERING AND TECHNOLOGY WAGHOLI PUNE</p>
   <p>Langauge Specalist in: php,sql,html,css</p>
   
   <p><button name=hetsheth >Contact</button></p>
 </div>
 <div class="card columns">
   <img src="image/img2.png" alt="John" style="width:100%">
-  <h1>Sharayu Mane</h1>
+  <h1>HRITHIK BANSAL</h1>
   <p class="title">Founder</p>
-  <p>College : BHAUSAHEB VARTAK POLYTECHNIC</p>
+  <p>College : G H RAISONI INSTITUTE OF ENGINEERING AND TECHNOLOGY WAGHOLI PUNE</p>
   <p>Langauge Specalist in: html,css</p>
   
   <p><button name=hetsheth >Contact</button></p>
 </div>
 <div class="card columns">
   <img src="image/img.jpg" alt="John" style="width:100%">
-  <h1>Avinash Singh</h1>
-  <p class="title">Founder</p>
-  <p>College : BHAUSAHEB VARTAK POLYTECHNIC</p>
-  <p>Langauge Specalist in: html,css</p>
-  
-  <p><button name=hetsheth >Contact</button></p>
-</div>
-<div class="card columns">
-  <img src="image/img.jpg" alt="John" style="width:100%">
-  <h1>Ganesh Kode</h1>
+  <h1>GANESH KODE</h1>
   <p class="title">Co-Founder</p>
-  <p>College : BHAUSAHEB VARTAK POLYTECHNIC</p>
+  <p>College : G H RAISONI INSTITUTE OF ENGINEERING AND TECHNOLOGY WAGHOLI PUNE</p>
   <p>Langauge Specalist in: sql,php,css,html</p>
   
   <p><button name=yashttailor>Contact</button></p>
-</div>
-<div class="card columns">
-  <img src="image/img.jpg" alt="John" style="width:100%">
-  <h1>Khan Hasan Raza</h1>
-  <p class="title">Co-Founder</p>
-  <p>College : BHAUSAHEB VARTAK POLYTECHNIC</p>
-  <p> Langauge Specalist in: sql,html,css,php</p>
-  <p><button name=swapnilsomawanshi>Contact</button></a></p>
 </div>
 </form>
  </div>
